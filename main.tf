819918e7f5cddf3145e0756808b2034d2c8bfcea
hgochev <hgochev@progress.com>
2026-07-17 12:15:08 +0300

    Create a VPC resource and add terraform checks

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 6.55.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+resource "aws_vpc" "main" {
+  region     = var.region
+  cidr_block = "10.0.0.0/16"
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  type        = string
+  description = "The AWS region to deploy resources in."
+  default     = "eu-north-1"
+}
