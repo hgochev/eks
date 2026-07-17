a342a16613eac3f8cf0017114d07899ccc9358d5
hgochev <hgochev@progress.com>
2026-07-17 15:35:13 +0300

    Format terraform

--- main.tf
+++ main.tf
@@ -323,9 +323,9 @@ resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.hg_eks_cluster.name
   node_group_name = "hg-eks-node-group"
   node_role_arn   = aws_iam_role.node_group.arn
-  subnet_ids     = values(aws_subnet.private)[*].id
-  instance_types = ["t3.medium"]
-  ami_type       = "AL2023_x86_64_STANDARD"
+  subnet_ids      = values(aws_subnet.private)[*].id
+  instance_types  = ["t3.medium"]
+  ami_type        = "AL2023_x86_64_STANDARD"
 
   disk_size = 20
 
