a8a1a834a78cc71f0a7402972d48b8965cbd6b03
hgochev <hgochev@progress.com>
2026-07-17 12:31:43 +0300

    Create aks subnets

--- main.tf
+++ main.tf
@@ -8,11 +8,27 @@ terraform {
   required_version = ">= 1.5.0"
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "eks_vpc" {
   region     = var.region
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "eks-vpc-${var.region}"
   }
+}
+
+data "aws_availability_zones" "available" {
+  region = var.region
+  state = "available"
+}
+
+locals {
+  azs = data.aws_availability_zones.available.names
+}
+
+resource "aws_subnet" "eks_subnet" {
+  for_each = toset(local.azs)
+  vpc_id              = aws_vpc.eks_vpc.id
+  availability_zone   = each.value
+  cidr_block          = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, index(local.azs, each.value))
 }
